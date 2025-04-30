7b808613e1e19722a94a85fdaf47e62e4699a26c
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-04-30 16:33:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,51 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+const video = document.querySelector('.flex');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const volume = document.querySelector('input[name="volume"]');
+const playback = document.querySelector('input[name="playbackSpeed"]');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
+
+// Toggle Play/Pause
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+function updateButton(){
+	toggle.textContent = video.paused ?' ►':'❚ ❚ '
+}
+
+function skip(){
+	video.currentTime+=parseFloat(this.dataset.skip);
+}
+
+function handleRangeupdate(){
+	video[this.name]=this.value;
+}
+
+function handleProgress(){
+	const percent = (video.currentTime /video.duration)*100;
+	progressFilled.style.flexBasis=`${percent}%`;
+}
+
+function scrub(e){
+	const scrubTime = (e.offsetX /  progress.offsetWidth) *(video.duration);
+	video.currentTime = scrubTime;
+}
+
+video.addEventListener("click",togglePlay)
+video.addEventListener("play",updateButton)
+video.addEventListener("pause",updateButton)
+video.addEventListener("timeupdate",handleProgress)
+ 
+skipButtons.forEach(btn => btn.addEventListener('click',skip));
+toggle.addEventListener("click",togglePlay);
+volume.addEventListener('input',handleRangeupdate);
+playback.addEventListener('input',handleRangeupdate);
+progress.addEventListener('click',scrub);
+
+
